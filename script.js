d2d639026d71b957cfd30d6a5612516d7526aa6b
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-24 15:23:25 +0200

    struttura

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var myObject = new Vue({ //creazione dell'oggetto Vue
     el: '#app', //Nome id
     data: {
-        message: 'Hello World!',
-        text: '',
-        img:'https://via.placeholder.com/150'
+        message: 'Hello World!', //stampa hello world su un h1 {{message}}
+        text: '', //prende simultaneo da un input:text e lo carica nel h1 {{text}}
+        img:'https://via.placeholder.com/150' //caricamento link SRC:img
     } //dove e cosa Stampare
 })
